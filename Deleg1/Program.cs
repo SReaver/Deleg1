ef2c7b782dbb2b928f8182552807894560f5cd97
SReaver <s.reaver@outlook.com>
2017-08-02 01:43:00 +0600

    Add project files.

---
+++ Deleg1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deleg1
+{
+    class Program
+    {
+        delegate double Multiply(double a, double b, bool y);
+
+        public static double Mult(double a, double b, bool y)
+        {
+            if (y)
+            {
+                //Console.WriteLine("Умножение");
+                return a * b;
+            }
+            else 
+            {
+                //Console.WriteLine("Сумма");
+                return a + b;
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Multiply M;
+            M = Mult;
+            Console.WriteLine(M(45.3,34.7,false));
+           
+        }
+    }
+}
